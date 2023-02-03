07062537dd794b18822c87e5f7a278d2426df338
Mike Stall <jmstall@microsoft.com>
2023-02-03 09:00:28 -0800

    remove

--- t.bat
+++
@@ -1,7 +0,0 @@
-copy /y D:\dev\temp\Power-Fx-Dataverse\src\PowerFx.Dataverse\bin\Debug\*.nupkg D:\dev\temp\Power-Fx-Dataverse\feed
-copy /y D:\dev\temp\Power-Fx-Dataverse\src\PowerFx.Dataverse.Eval\bin\Debug\*.nupkg D:\dev\temp\Power-Fx-Dataverse\feed
-
-copy /y D:\dev\temp\Power-Fx-Dataverse\src\PowerFx.Dataverse.Eval\bin\Debug\netstandard2.0\*.pdb D:\dev\temp\Power-Fx-Dataverse\feed
-
-rmdir /S /Q C:\Users\jmstall\.nuget\packages\microsoft.powerfx.dataverse.eval\1.0.0
-rmdir /S /Q C:\Users\jmstall\.nuget\packages\microsoft.powerfx.dataverse\1.0.0
