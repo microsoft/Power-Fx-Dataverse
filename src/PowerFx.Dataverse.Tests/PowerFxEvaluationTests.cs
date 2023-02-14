42e5e56171e7e7b878cdd91246cbe3ab4079be9d
anderson-joyle <joyle@outlook.com>
2023-02-14 10:57:14 -0600

    Disabling tabular test cases

--- src/PowerFx.Dataverse.Tests/PowerFxEvaluationTests.cs
+++ src/PowerFx.Dataverse.Tests/PowerFxEvaluationTests.cs
@@ -52,7 +52,7 @@ namespace Microsoft.PowerFx.Dataverse.Tests
 
                 // Ideally, this should only go down as the rest of the functions/capabilities are added
                 // TODO: replace error count with locally based overlays of specific differences
-                Assert.AreEqual(138, result.Fail);
+                Assert.AreEqual(100, result.Fail);
             }
         }
 
