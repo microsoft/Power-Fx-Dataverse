69aa48406db4ced4ad096ba8f78841bb5c301ab4
Mike Stall <jmstall@microsoft.com>
2023-03-22 15:08:00 -0700

    batch files for building local nugets

---
+++ src/buildAndRefresh.bat
@@ -0,0 +1,13 @@
+pwsh.exe -executionpolicy bypass -file %~dp0refreshLocalNugetCache.ps1
+
+copy /y %~dp0PowerFx.Dataverse\bin\Debug\*.nupkg %~dp0outputpackages
+copy /y %~dp0PowerFx.Dataverse.Eval\bin\Debug\*.nupkg  %~dp0outputpackages
+
+@dir %~dp0outputpackages
+
+@echo To consume these nugets locally:
+@echo 1) Add this to your nuget.config:
+@echo  ^<add key="LocalFxDataverse" value="%~dp0outputpackages" /^>
+@echo .
+@echo 2) Set your current PowerFx version to
+@echo  0.3.0-local
---
+++ src/refreshLocalNugetCache.ps1
@@ -0,0 +1,11 @@
+﻿$localCache = $Env:USERPROFILE + "\.nuget\packages"
+
+Write-Host "Deleting Microsoft.PowerFx.Dataverse*\1.0.0 from nuget cache. This will cause the newly built packages to be downloaded."
+
+Get-ChildItem $localCache Microsoft.PowerFx.Dataverse.* | ForEach-Object {
+    $path = ($_.FullName + "\1.0.0")
+
+    if (Test-Path $path) {
+      Remove-Item $path -Recurse
+    }
+}
