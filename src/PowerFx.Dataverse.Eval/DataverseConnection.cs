9bfa8286ef3dd03bcb781f2f10f75a686382dce6
jas-valgotar <jasvalgotar@microsoft.com>
2023-04-20 17:51:08 -0400

    fixes test.

--- src/PowerFx.Dataverse.Eval/DataverseConnection.cs
+++ src/PowerFx.Dataverse.Eval/DataverseConnection.cs
@@ -35,6 +35,8 @@ namespace Microsoft.PowerFx.Dataverse
         public string GetSerializationName(string tableLogicalName);
 
         public RecordType GetRecordType(string tableLogicalName);
+
+        public RecordValue Marshal(Entity entity);
     }
 
     /// <summary>
