1bdb70e622402fef2e9ffa611e1a0ba62544b156
jas-valgotar <jasvalgotar@microsoft.com>
2023-05-09 11:40:01 -0400

    Enables delegation for FirstN

---
+++ src/PowerFx.Dataverse/Eval/DelegatedFunctions/DelegatedFirstNFunction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.PowerFx.Core.IR;
+using Microsoft.PowerFx.Types;
+using Microsoft.Xrm.Sdk.Query;
+using System.Threading;
+using System.Threading.Tasks;
+using static Microsoft.PowerFx.Dataverse.DelegationEngineExtensions;
+
+namespace Microsoft.PowerFx.Dataverse
+{
+    // Generate a lookup call for: __Lookup(Table, Id=Guid)  
+    internal class DelegatedFirstNFunction : DelegateFunction
+    {
+        public DelegatedFirstNFunction(DelegationHooks hooks, TableType tableType)
+          : base(hooks, "__top", tableType, tableType, FormulaType.Number)
+        {
+        }
+
+        public override async Task<FormulaValue> InvokeAsync(FormulaValue[] args, CancellationToken cancellationToken)
+        {
+            var table = (TableValue)args[0];
+            var topCount = ((NumberValue)args[1]).Value;
+
+            var rows = await _hooks.RetrieveMultipleAsync(table, filter: null, (int)topCount, cancellationToken);
+
+            var result = new InMemoryTableValue(IRContext.NotInSource(this.ReturnFormulaType), rows);
+
+            return result;
+        }
+    }
+}
