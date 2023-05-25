d0e706faa7334a76571fc5cfe8d6b9db0678ea39
anderson-joyle <joyle@outlook.com>
2023-05-25 10:32:17 -0500

    Absorvb latest PFX changes

--- src/PowerFx.Dataverse/DataSource/DataverseDataSourceInfo.cs
+++ src/PowerFx.Dataverse/DataSource/DataverseDataSourceInfo.cs
@@ -186,6 +186,9 @@ namespace Microsoft.PowerFx.Dataverse
         string IDataEntityMetadata.OriginalDataDescriptionJson => throw new NotImplementedException();
 
         string IDataEntityMetadata.InternalRepresentationJson => throw new NotImplementedException();
+
+        public bool IsRefreshable => throw new NotImplementedException();
+
         void IDataEntityMetadata.LoadClientSemantics(bool isPrimaryTable)
         {
             throw new NotImplementedException();
