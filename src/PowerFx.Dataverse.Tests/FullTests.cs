b6ff869b3981380a4ea05e81871991ed344b94ce
Neeraja T <neerajat@microsoft.com>
2023-05-30 11:22:04 +0530

    fixed testcase

--- src/PowerFx.Dataverse.Tests/FullTests.cs
+++ src/PowerFx.Dataverse.Tests/FullTests.cs
@@ -270,7 +270,7 @@ namespace Microsoft.PowerFx.Dataverse.Tests
 
                 ExecuteSqlTest("Rating = 'Rating (Thises)'.Hot", false, cx, new EntityMetadataModel[] { metadata });
                 ExecuteSqlTest("Rating <> 'Rating (Thises)'.Hot", true, cx, new EntityMetadataModel[] { metadata });
-                ExecuteSqlTest("If(1 > 2, 'Rating (Thises)'.Hot, 'Rating (Thises)'.Warm)", 2M, cx, new EntityMetadataModel[] { metadata });
+                ExecuteSqlTest("If(1 > 2, 'Rating (Thises)'.Hot, 'Rating (Thises)'.Warm)", 2, cx, new EntityMetadataModel[] { metadata });
             }
         }
 
