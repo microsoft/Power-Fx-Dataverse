49f2fac30a86ef62c96e0f0beb0a878120456948
Hoa Nguyen <nguhoa@microsoft.com>
2023-07-12 11:57:51 -0700

    Apply Text format extraction from Interpreter to SQL Text() and remove the unnecessary tests in *override.txt

--- src/PowerFx.Dataverse/Functions/StringFunctions.cs
+++ src/PowerFx.Dataverse/Functions/StringFunctions.cs
@@ -6,12 +6,16 @@
 
 using Microsoft.AppMagic.Common;
 using Microsoft.PowerFx.Core.IR;
+using Microsoft.PowerFx.Core.IR.Nodes;
+using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Types;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using Microsoft.PowerFx.Core.IR.Nodes;
-using Microsoft.PowerFx.Types;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
 using static Microsoft.PowerFx.Dataverse.SqlVisitor;
 using Microsoft.PowerFx.Dataverse.CdsUtilities;
 
@@ -110,12 +114,23 @@ namespace Microsoft.PowerFx.Dataverse.Functions
                     // The numeric formating placeholders for Text: https://docs.microsoft.com/en-us/powerapps/maker/canvas-apps/functions/function-text#number-placeholders
                     // generally match the .NET placeholders used by SQL: https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-numeric-format-strings
 
-                    // Do not allow , . or locale tag
-                    if (Regex.IsMatch(format, @"(,|\.|\[\$-[\w-]+\])"))
+                    var textFormatArgs = new TextFormatArgs
+                    {
+                        FormatCultureName = null,
+                        FormatArg = null,
+                        HasDateTimeFmt = false,
+                        HasNumericFmt = false
+                    };
+
+                    // Do not allow , . or locale tag or datetime format
+                    if (!TextFormatUtils.IsValidFormatArg(format, formatCulture: null, defaultLanguage: null, out textFormatArgs) || Regex.IsMatch(textFormatArgs.FormatArg, @"(,|\.)")
+                        || textFormatArgs.HasDateTimeFmt || !string.IsNullOrEmpty(textFormatArgs.FormatCultureName))
                     {
                         context._unsupportedWarnings.Add("Unsupported numeric format");
                         throw new SqlCompileException(SqlCompileException.NumericFormatNotSupported, node.Args[1].IRContext.SourceContext);
                     }
+
+                    format = textFormatArgs.FormatArg;
 
                     // except that % ‰ e : ' need to be escaped
                     format = format.Replace("%", "\\%");
