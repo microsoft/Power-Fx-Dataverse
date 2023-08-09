a5a4f25c1cb4b01a7ced3f47ab04abf1bc34847a
anderson-joyle <joyle@outlook.com>
2023-08-09 16:13:03 -0500

    Latest PFx version.

--- src/PowerFx.Dataverse.Tests/PluginExecutionTests/PluginExecutionTests.cs
+++ src/PowerFx.Dataverse.Tests/PluginExecutionTests/PluginExecutionTests.cs
@@ -2597,12 +2597,12 @@ namespace Microsoft.PowerFx.Dataverse.Tests
 
         [Theory]
         [InlineData("LookUp(t1, LocalId=If(Price>50, _g1, _gMissing)).Price",
-            "Warning 22-27: Can't delegate LookUp: Expression compares multiple fields.")]
+            "Warning 22-27: Não é possível delegar LookUp: a expressão compara vários campos.")]
         [InlineData("LookUp(t1, LocalId=LocalId).Price",
-            "Warning 18-19: This predicate will always be true. Did you mean to use ThisRecord or [@ ]?",
-            "Warning 19-26: Can't delegate LookUp: Expression compares multiple fields.")]
+            "Warning 18-19: Este predicado será sempre verdadeiro. Você quis usar ThisRecord ou [@ ]?",
+            "Warning 19-26: Não é possível delegar LookUp: a expressão compara vários campos.")]
         [InlineData("LookUp(Filter(t1, 1=1), localid=_g1).Price",
-            "Warning 14-16: This operation on table 'local' may not work if it has more than 999 rows."
+            "Warning 14-16: Esta operação na tabela \"local\" poderá não funcionar se tiver mais de 999 linhas."
             )]
         public void LookUpDelegationWarningLocaleTest(string expr, params string[] expectedWarnings)
         {
