61b08b5a6ab0a5d7016e88c37c6c10bc4596ddfe
Aashman <aashmangupta@gmail.com>
2023-08-14 19:31:50 -0400

    Added AIReply & AISentiment functions (#304)
    added AIReply & AISentiment
    
    Co-authored-by: Aashman Gupta <aashmangupta@microsoft.com>

---
+++ src/PowerFx.Dataverse.Eval/Functions/AIReplyFunction.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.PowerFx;
+using Microsoft.PowerFx.Interpreter;
+using Microsoft.PowerFx.Types;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.PowerFx.Dataverse
+{
+    // AIReply(String) : string 
+    // given a string, call GPT to return a response to the string. 
+    public class AIReplyFunction : ReflectionFunction
+    {
+        public AIReplyFunction()
+        {
+            this.ConfigType = typeof(IDataverseExecute);
+        }
+
+        // POCO for the OrganizationRequest message.
+        private class ReplyRequest
+        {
+            /// <summary>
+            /// The incoming text/question. 
+            /// </summary>
+            public string Text { get; set; }
+
+            public OrganizationRequest Get()
+            {
+                var req = new OrganizationRequest("AIReply");
+                req[nameof(ReplyRequest.Text)] = this.Text;
+
+                return req;
+            }
+        }
+
+        // POCO for the OrganizationResponse message.
+        private class ReplyResponse
+        {
+            public string PreparedResponse { get; set; }
+
+            public static ReplyResponse Parse(OrganizationResponse res)
+            {
+                res.ValidateNameOrThrowEvalEx("AIReply");
+
+                var str = res.Results.GetOrThrowEvalEx<string>(nameof(ReplyResponse.PreparedResponse));
+
+                return new ReplyResponse
+                {
+                    PreparedResponse = str
+                };
+            }
+        }
+
+        // Entry called by Power Fx interpreter. 
+        public async Task<StringValue> Execute(IDataverseExecute client, StringValue value, CancellationToken cancel)
+        {
+            if (client == null)
+            {
+                // We missed a call to AddDataverseExecute in the runtime config.
+                throw new CustomFunctionErrorException("Org not available");
+            }
+
+            var result = await RepliedText(value.Value, client, cancel);
+
+            return FormulaValue.New(result);
+        }
+
+        private async Task<string> RepliedText(string myText, IDataverseExecute service, CancellationToken cancel)
+        {
+            var req = new ReplyRequest
+            {
+                Text = myText
+            }.Get();
+
+            var result = await service.ExecuteAsync(req, cancel);
+            result.ThrowEvalExOnError();
+
+            var response = ReplyResponse.Parse(result.Response);
+
+            return response.PreparedResponse;
+        }
+    }
+}
---
+++ src/PowerFx.Dataverse.Eval/Functions/AISentimentFunction.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.PowerFx;
+using Microsoft.PowerFx.Interpreter;
+using Microsoft.PowerFx.Types;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.PowerFx.Dataverse
+{
+    // AISentiment(String) : string 
+    // given a string, call GPT to return the analyzed sentiment of the string. 
+    public class AISentimentFunction : ReflectionFunction
+    {
+        public AISentimentFunction()
+        {
+            this.ConfigType = typeof(IDataverseExecute);
+        }
+
+        // POCO for the OrganizationRequest message.
+        private class SentimentRequest
+        {
+            /// <summary>
+            /// The incoming text. 
+            /// </summary>
+            public string Text { get; set; }
+
+            public OrganizationRequest Get()
+            {
+                var req = new OrganizationRequest("AISentiment");
+                req[nameof(SentimentRequest.Text)] = this.Text;
+
+                return req;
+            }
+        }
+
+        // POCO for the OrganizationResponse message.
+        private class SentimentResponse
+        {
+            public string AnalyzedSentiment { get; set; }
+
+            public static SentimentResponse Parse(OrganizationResponse res)
+            {
+                res.ValidateNameOrThrowEvalEx("AISentiment");
+
+                var str = res.Results.GetOrThrowEvalEx<string>(nameof(SentimentResponse.AnalyzedSentiment));
+
+                return new SentimentResponse
+                {
+                    AnalyzedSentiment = str
+                };
+            }
+        }
+
+        // Entry called by Power Fx interpreter. 
+        public async Task<StringValue> Execute(IDataverseExecute client, StringValue value, CancellationToken cancel)
+        {
+            if (client == null)
+            {
+                // We missed a call to AddDataverseExecute in the runtime config.
+                throw new CustomFunctionErrorException("Org not available");
+            }
+
+            var result = await AnalyzedSentimentText(value.Value, client, cancel);
+
+            return FormulaValue.New(result);
+        }
+
+        private async Task<string> AnalyzedSentimentText(string myText, IDataverseExecute service, CancellationToken cancel)
+        {
+            var req = new SentimentRequest
+            {
+                Text = myText
+            }.Get();
+
+            var result = await service.ExecuteAsync(req, cancel);
+            result.ThrowEvalExOnError();
+
+            var response = SentimentResponse.Parse(result.Response);
+
+            return response.AnalyzedSentiment;
+        }
+    }
+}
