f1399621c5c7c28dc3074850b0245f0337fc342d
anderson-joyle <joyle@outlook.com>
2023-08-15 12:12:19 -0500

    PR feedback.

--- src/PowerFx.Dataverse.Tests/PluginExecutionTests/PluginExecutionTests.cs
+++ src/PowerFx.Dataverse.Tests/PluginExecutionTests/PluginExecutionTests.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.ServiceModel;
 using System.Text.Json;
 using System.Threading;
