b28cf16297acd8f2efbf24bfef97db0f671695c2
Mike Stall <mikestall@hotmail.com>
2023-10-20 11:14:00 -0700

    Docs for what SQL compiler implements.  (#387)
    * add
    
    * usings
    
    ---------
    
    Co-authored-by: Mike Stall <jmstall@microsoft.com>

---
+++ src/PowerFx.Dataverse.Tests/Docs/FunctionNameTests.cs
@@ -0,0 +1,74 @@
+﻿//------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.PowerFx.Core.Tests;
+using Xunit;
+
+namespace Microsoft.PowerFx.Dataverse.Tests
+{
+
+    public class FunctionNameTests
+    {
+        private static EngineSchema GetActual()
+        {
+            var funcs = Dataverse.Functions.Library.FunctionList;
+            var set = new HashSet<string>(funcs.Select(f => f.Name));
+
+            // Some functions exist in the list just so they can bind and return failure. 
+            // These are not actually supported. 
+            set.Remove("IsToday");
+            set.Remove("Today");
+
+            var actualSchema = new EngineSchema
+            {
+                FunctionNames = set.ToArray()
+            }.Normalize();
+
+            return actualSchema;
+        }
+
+        private static EngineSchema Read(string schemaName)
+        {
+            var path = Path.GetFullPath(schemaName);
+            var json = File.ReadAllText(path);
+            var schema = JsonSerializer.Deserialize<EngineSchema>(json).Normalize();
+
+            return schema;
+        }
+
+        // Verify that the .json file describing which functions SQL compiler implements.
+        // When you add/remove functions from Library.cs, need to update this json. 
+        // Doc team then pulls from this json to drive public docs. 
+        [Fact]
+        public void SqlCompilerSchemaCheck()
+        {
+            // Get checked in schema 
+            string schemaName = @"Docs\DataverseFormulaColumns.json";
+
+            var expectedSchema = Read(schemaName);
+
+            var actualSchema = GetActual();
+
+            // Write actual for easy debugger 
+            var pathTemp = Path.Combine(Path.GetTempPath(), "actual-" + Path.GetFileName(schemaName));
+            var jsonActual = JsonSerializer.Serialize(actualSchema, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            File.WriteAllText(pathTemp, jsonActual);
+
+            // Compare
+            var expected = expectedSchema.GetCompareString();
+            var actual = actualSchema.GetCompareString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
