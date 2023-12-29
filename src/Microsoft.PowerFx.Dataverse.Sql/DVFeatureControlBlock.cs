5231a73b6152f24964d01d2304f5cb43377d997b
Sanjeev Goyal <123401931+sanjeevgoyalmsft@users.noreply.github.com>
2023-12-29 15:25:30 +0530

    Update DVFeatureControlBlock.cs

--- src/Microsoft.PowerFx.Dataverse.Sql/DVFeatureControlBlock.cs
+++ src/Microsoft.PowerFx.Dataverse.Sql/DVFeatureControlBlock.cs
@@ -2,6 +2,10 @@
 {
     public sealed class DVFeatureControlBlock
     {
+        // This FCB is to enable/disable Floating Point Feature, when this flag is enabled, Power Fx Core Number Data type will be treated as Double/Float and
+        // Formula Columns will be able to produce/consume float data type columns
+        // when this flag is disabled, Number Data type will act as Power Fx Core Decimal Data type and all Number Expressions would be treated as decimal 
+        // and Formula columns will not be able to produce/consumer float type columns
         internal bool IsFloatingPointEnabled { get; set;  }
     }
 }
