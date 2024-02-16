078887bb4794bdb66bc079c94469ff47d71424d8
Sanjeev Goyal <123401931+sanjeevgoyalmsft@users.noreply.github.com>
2024-02-16 11:20:08 +0530

    Update DataverseFeatures.cs (#440)

--- src/Microsoft.PowerFx.Dataverse.Sql/DataverseFeatures.cs
+++ src/Microsoft.PowerFx.Dataverse.Sql/DataverseFeatures.cs
@@ -6,6 +6,6 @@
         // Formula Columns will be able to produce/consume float data type columns
         // when this flag is disabled, Number Data type will act as Power Fx Core Decimal Data type and all Number Expressions would be treated as decimal 
         // and Formula columns will not be able to produce/consumer float type columns
-        internal bool IsFloatingPointEnabled { get; set;  }
+        public bool IsFloatingPointEnabled { get; set;  }
     }
 }
