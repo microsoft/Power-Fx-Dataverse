6d13f9ebceb90536399fb6c16f7122abb1067aa5
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2024-04-05 11:43:54 -0400

    Polymorphic helper. (#465)

---
+++ src/PowerFx.Dataverse.Tests/UtilityTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Dataverse.EntityMock;
+using Xunit;
+
+namespace Microsoft.PowerFx.Dataverse.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void IsFieldPolymorphicTest()
+        {
+            var map = new AllTablesDisplayNameProvider();
+            map.Add("local", "t1");
+            var policy = new SingleOrgPolicy(map);
+
+            (DataverseConnection dv, EntityLookup el) = PluginExecutionTests.CreateMemoryForRelationshipModels(numberIsFloat: true, policy: policy);
+
+            var entityMetadata = dv.GetMetadataOrThrow("local");
+
+            // OData name.
+            Assert.True(entityMetadata.IsFieldPolymorphic("new_polyfield"));
+
+            // Logical name.
+            Assert.True(entityMetadata.IsFieldPolymorphic("_new_polyfield_value"));
+        }
+    }
+}
