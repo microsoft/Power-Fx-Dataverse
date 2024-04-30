a2502ef0959f7be7f1fd6838192b8941412963d1
Luc Genetier <lucgen@microsoft.com>
2024-04-30 14:21:37 +0200

    Update

--- src/PowerFx.Dataverse/XrmUtility.cs
+++ src/PowerFx.Dataverse/XrmUtility.cs
@@ -1,5 +1,8 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+﻿//------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
 
 using System;
 using System.Collections.Generic;
