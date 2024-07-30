2de917fec8e74a20a79f38557aef4a1e1b51b827
Luc Genetier <lucgen@microsoft.com>
2024-07-30 18:11:59 +0200

    Add more tests

--- src/PowerFx.Dataverse.Tests/DelegationTests/ForAllDelegationTests.cs
+++ src/PowerFx.Dataverse.Tests/DelegationTests/ForAllDelegationTests.cs
@@ -56,6 +56,8 @@ namespace Microsoft.PowerFx.Dataverse.Tests.DelegationTests
         [InlineData(38, "ForAll(ForAll(t1, Price*2), Value )", 4, "Value", "200, 20, -20, 20", "Warning 14-16: This operation on table 'local' may not work if it has more than 999 rows.")]
         [InlineData(39, @"ForAll(t1, Value(Price, ""fr-FR""))", 4, "Value", "100, 10, -10, 10", "Warning 7-9: This operation on table 'local' may not work if it has more than 999 rows.")]
         // [InlineData(40, "ForAll(t1, ThisRecord)", 4, "Price", "100, 10, -10, 10")] // Cannot add test as PrettyPrintIRVisitor throws NotImplementedException
+        [InlineData(41, "ForAll(Distinct(Filter(t1, Price > 0), Price * 2), Value)", 2, "Value", "200, 20")]
+        [InlineData(42, "Distinct(ForAll(Filter(t1, Price > 0), Price * 2), Value)", 2, "Value", "200, 20")]
         public async Task ForAllDelegationAsync(int id, string expr, int expectedRows, string column, string expectedIds, params string[] expectedWarnings)
         {
             await DelegationTestAsync(id, "ForAllDelegation.txt", expr, expectedRows, expectedIds,
