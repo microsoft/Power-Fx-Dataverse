de1a512e2b953f016c143f78c00af77fd34f58e0
Luc Genetier <lucgen@microsoft.com>
2024-08-16 15:47:21 +0200

    Update

--- src/PowerFx.Dataverse/Eval/Delegation/NodeVisitors/CallNodeVisitor.cs
+++ src/PowerFx.Dataverse/Eval/Delegation/NodeVisitors/CallNodeVisitor.cs
@@ -73,7 +73,7 @@ namespace Microsoft.PowerFx.Dataverse
             // Only below function fulfills assumption that first arg is Table
             if (!(node.Function.ParamTypes.Length > 0 && node.Function.ParamTypes[0].IsTable))
             {
-                return base.Visit(node, context);
+                return new RetVal(node);                
             }
 
             RetVal tableArg;
