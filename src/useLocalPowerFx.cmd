126cf070fb738081c830828011ed3b615913355c
Luc Genetier <lucgen@microsoft.com>
2024-09-25 17:54:14 +0200

    Update

--- src/useLocalPowerFx.cmd
+++ src/useLocalPowerFx.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set nugetRoot=C:\Users\lucgen\.nuget\packages
-set pfxVer=1.3.0-build.20240917-0002
+set pfxVer=1.3.0-build.20240918-1004
 set pfxRoot=C:\Data\Power-Fx
 set pfxConfig=Debug
 
