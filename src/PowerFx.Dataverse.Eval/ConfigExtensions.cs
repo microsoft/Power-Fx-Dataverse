ce9a3be13cb496973b50ca5fb72e912a1645cb9c
anderson-joyle <joyle@outlook.com>
2024-10-10 13:55:00 -0500

    Fixes.

--- src/PowerFx.Dataverse.Eval/ConfigExtensions.cs
+++ src/PowerFx.Dataverse.Eval/ConfigExtensions.cs
@@ -9,9 +9,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.PowerFx;
+using Microsoft.PowerFx.Core;
 using Microsoft.PowerFx.Interpreter;
 using Microsoft.PowerFx.Types;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace Microsoft.PowerFx.Dataverse
 {
@@ -56,6 +58,29 @@ namespace Microsoft.PowerFx.Dataverse
         public static void AddDataverseExecute(this RuntimeConfig config, IDataverseExecute clientExecute)
         {
             config.AddService<IDataverseExecute>(clientExecute);
+        }
+
+        public static async Task<RecordValue> GetEnvironmentVariablesAsync(this IDataverseReader reader)
+        {
+            var filter = new FilterExpression();
+
+            // Data source and Secret types are not supported.
+            filter.FilterOperator = LogicalOperator.Or;
+            filter.AddCondition("type", ConditionOperator.Equal, (int)EnvironmentVariableType.Decimal);
+            filter.AddCondition("type", ConditionOperator.Equal, (int)EnvironmentVariableType.String);
+            filter.AddCondition("type", ConditionOperator.Equal, (int)EnvironmentVariableType.JSON);
+            filter.AddCondition("type", ConditionOperator.Equal, (int)EnvironmentVariableType.Boolean);
+
+            var definitions = await reader.RetrieveMultipleAsync<EnvironmentVariableDefinitionEntity>(filter, CancellationToken.None);
+            var logicalToDisplayNames = new List<KeyValuePair<string, string>>();
+
+            foreach (var definition in definitions)
+            {
+                logicalToDisplayNames.Add(new KeyValuePair<string, string>(definition.schemaname, definition.displayname));
+            }
+
+            return new DataverseEnvironmentVariablesRecordValue(
+                new DataverseEnvironmentVariablesRecordType(DisplayNameUtility.MakeUnique(logicalToDisplayNames), definitions), reader);
         }
     }
 }
