0f28f3e78955430e67c829101294cb066d5ce6c4
Mike Stall (from Dev Box) <mikestall@hotmail.com>
2024-11-16 09:24:16 -0800

    more tests

---
+++ src/PowerFx.Dataverse.Tests/CoreValidation.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.PowerFx.Types;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Microsoft.PowerFx.Dataverse.Tests.DelegationTests
+{
+    // Sanity check assumptions from incoming Microsoft.PowerFx.Core nuget. 
+    // If these ever fail - soemthing is fundamentally very wrong. 
+    public class CoreValidation
+    {
+        [Fact]
+        public void ConfigTest()
+        {
+            var config = new PowerFxConfig();
+            Assert.True(config.Features.SupportColumnNamesAsIdentifiers);
+        }
+    }
+}
