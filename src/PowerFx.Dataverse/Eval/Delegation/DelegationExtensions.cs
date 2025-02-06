142604756000124e925d882d3e406954c45422f5
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2025-02-06 13:02:47 -0500

    Update Fx (#597)

--- src/PowerFx.Dataverse/Eval/Delegation/DelegationExtensions.cs
+++ src/PowerFx.Dataverse/Eval/Delegation/DelegationExtensions.cs
@@ -8,12 +8,7 @@ namespace Microsoft.PowerFx.Dataverse.Eval.Delegation
 {
     internal static class DelegationExtensions
     {
-        public static bool CanDelegateSummarize(this TableDelegationInfo tableDelegationInfo, FxColumnInfo columnInfo, SummarizeMethod method, bool isDataverseDelegation)
-        {
-            return tableDelegationInfo.CanDelegateSummarize(columnInfo, isDataverseDelegation) && tableDelegationInfo.CanDelegateSummarize(method, isDataverseDelegation);
-        }
-
-        public static bool CanDelegateSummarize(this TableDelegationInfo tableDelegationInfo, FxColumnInfo columnInfo, bool isDataverseDelegation)
+        public static bool CanDelegateSummarize(this TableDelegationInfo tableDelegationInfo, string columnName, SummarizeMethod summarizeMethod, bool isDataverseDelegation)
         {
             if (isDataverseDelegation)
             {
@@ -26,28 +21,7 @@ namespace Microsoft.PowerFx.Dataverse.Eval.Delegation
             }
 
             var summarizeCapabilities = tableDelegationInfo.SummarizeCapabilities;
-            if (summarizeCapabilities.IsSummarizableProperty(columnInfo.RealColumnName))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        public static bool CanDelegateSummarize(this TableDelegationInfo tableDelegationInfo, SummarizeMethod method, bool isDataverseDelegation)
-        {
-            if (isDataverseDelegation)
-            {
-                return true;
-            }
-
-            if (tableDelegationInfo == null || tableDelegationInfo.SummarizeCapabilities == null)
-            {
-                return false;
-            }
-
-            var summarizeCapabilities = tableDelegationInfo.SummarizeCapabilities;
-            if (summarizeCapabilities.IsSummarizableMethod(method))
+            if (summarizeCapabilities.IsSummarizableProperty(columnName, summarizeMethod))
             {
                 return true;
             }
